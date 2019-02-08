a98bce83d2ee89ca1cc7f836bc372976bb0dcb97
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-02-08 09:03:52 +0100

    Add project files.

---
+++ ConsoleXUnitWorkshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleXUnitWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
